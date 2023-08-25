d2bc9c5a6f54b09aafe5283844e72f015ada3ce6
wrote-code <developer@noreply.email.com>
2023-08-25 19:35:55 +0800

    修改sql，开启事务注解

--- src/main/java/com/wrotecode/spring/springtransaction/controller/UserController.java
+++ src/main/java/com/wrotecode/spring/springtransaction/controller/UserController.java
@@ -7,6 +7,7 @@ import com.wrotecode.spring.springtransaction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,7 +65,7 @@ public class UserController {
         return service.deleteContact(contactId);
     }
 
-    @PostMapping("/queryAll")
+    @GetMapping("/queryAll")
     public List queryAll() {
         return service.queryAll();
     }
